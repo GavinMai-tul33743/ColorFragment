1dd9da4d816724148621444f1b16eebbfcb8047c
Gavin Mai <tul33743@temple.edu>
2024-03-12 12:15:22 -0400

    added color fragment

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
